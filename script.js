287fbd35c1771ca7401c1641ab22412ab38bb0d4
vrolijk <vrolijk@hotitem.nl>
2023-09-27 16:05:15 +0200

    Added leaflet-gpx resource

--- script.js
+++ script.js
@@ -7,12 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Voeg de GPX-track toe aan de kaart
 var gpx = 'gpx/route_1_alex_tincave.gpx'; // Vervang dit met de naam van je GPX-bestand
 new L.GPX(gpx, {
-    async: true,
-    marker_options: {
-        startIconUrl: 'https://cdn.jsdelivr.net/npm/leaflet-gpx@1.7.0/pin-icon-start.png',
-        endIconUrl: 'https://cdn.jsdelivr.net/npm/leaflet-gpx@1.7.0/pin-icon-end.png',
-        shadowUrl: 'https://cdn.jsdelivr.net/npm/leaflet-gpx@1.7.0/pin-shadow.png'
-      }
+    async: true
 }).on('loaded', function (e) {
     map.fitBounds(e.target.getBounds());
 }).addTo(map);
